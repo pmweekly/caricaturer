484885b2311ce09311189680a308e6ad1f15d357
ymm <ymm@ymmdeMac-mini.local>
2026-03-10 14:56:35 +0800

    chore: add sitemap and robots (static) and next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://caricaturer.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
